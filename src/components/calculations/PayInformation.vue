ce6a58de83cea2390d0736f9bed5c7691b2a83b5
IanTapply22 <contact@ian-tapply.me>
2024-06-30 23:57:44 -0400

    Update layout of the budgets information to be neater Fixed header not being light font style

--- src/components/calculations/PayInformation.vue
+++ src/components/calculations/PayInformation.vue
@@ -1,26 +1,40 @@
 <template>
-    <v-row>
-        <v-col>
-            <h2 class="text-center font-weight-light">Hourly Wage (CAD): {{ budget.hourlyWage }}</h2>
-        </v-col>
-        <v-col>
-            <h2 class="text-center font-weight-light">Hours Per Week: {{ budget.hoursPerWeek }}</h2>
-        </v-col>
-        <v-col>
-            <h2 class="text-center font-weight-light">Pay Schedule: {{ budget.paySchedule }}</h2>
-        </v-col>
-        <v-col>
-            <h2 class="text-center font-weight-light">Pay Tax Percentage: {{ budget.payTaxPercentage }}</h2>
-        </v-col>
+  <v-container>
+    <v-row justify="space-around">
+      <v-col cols="12" md="6" lg="3">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Hourly Wage (CAD): <strong>{{ budget.hourlyWage }}</strong></h2>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="3">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Hours Per Week: <strong>{{ budget.hoursPerWeek }}</strong></h2>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="3">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Pay Schedule: <strong>{{ budget.paySchedule }}</strong></h2>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="3">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Pay Tax Percentage: <strong>{{ budget.payTaxPercentage }}</strong></h2>
+        </v-card>
+      </v-col>
     </v-row>
-    <v-row>
-        <v-col>
-            <h2 class="text-center font-weight-light">Percentage To Spendings: {{ budget.percentageToSpending }}</h2>
-        </v-col>
-        <v-col>
-            <h2 class="text-center font-weight-light">Percentage To Savings: {{ budget.percentageToSaving }}</h2>
-        </v-col>
+    <v-row justify="space-around">
+      <v-col cols="12" md="6">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Percentage To Spendings: <strong>{{ budget.percentageToSpending }}</strong></h2>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="pa-3" outlined>
+          <h2 class="text-center font-weight-light">Percentage To Savings: <strong>{{ budget.percentageToSaving }}</strong></h2>
+        </v-card>
+      </v-col>
     </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
--- src/components/headers/BudgetsHeader.vue
+++ src/components/headers/BudgetsHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="ml-2 mt-4">
+    <div class="ml-2 mt-6">
         <h1 class="mt-4 text-center">Budgets</h1>
-        <h2 class="text-center font-weight-medium mb-4">Contains vital calculations that are important to creating a healthy
+        <h2 class="text-center font-weight-light mb-4">Contains vital calculations that are important to creating a healthy
             and vital budget based on your earnings.</h2>
     </div>
 </template>
